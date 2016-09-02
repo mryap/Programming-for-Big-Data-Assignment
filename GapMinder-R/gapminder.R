b46103c2a8cfb43c7b179cb60f55e64b1493b6b9
Mr. Yap <shyanyap@gmail.com>
2016-09-02 19:55:40 +0100

    Add files via upload

---
+++ GapMinder-R/gapminder.R
@@ -0,0 +1,19 @@
+# https://github.com/jennybc/gapminder
+
+install.packages("gapminder")
+
+library("gapminder")
+
+aggregate(lifeExp ~ continent, gapminder, median)
+
+suppressPackageStartupMessages(library("dplyr"))
+gapminder %>%
+  filter(year == 2007) %>%
+  group_by(continent) %>%
+  summarise(lifeExp = median(lifeExp))
+
+library("ggplot2")
+ggplot(gapminder, aes(x = continent, y = lifeExp)) +
+  geom_boxplot(outlier.colour = "hotpink") +
+  geom_jitter(position = position_jitter(width = 0.1, height = 0), alpha = 1/4)
+
